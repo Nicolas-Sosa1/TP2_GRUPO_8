911735652a4a3af36a6f3dbfa637269e3518ef89
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-09-02 17:54:58 -0300

    agrego constructor y metodos

--- src/Ejercicio2/Polideportivo.java
+++ src/Ejercicio2/Polideportivo.java
@@ -15,5 +15,28 @@ public class Polideportivo implements IInstalacionDeportiva, IEdificio{
 		
 	}
 	
+	public Polideportivo(String nombre, double superficie, int tipoDeInstalacion) {
+		this.nombre = nombre;
+		this.superficie = superficie;
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
+	
+
+	
+	public void setNombre(String nombre) { 
+		this.nombre = nombre; 
+		}
+	
+	public String getNombre() { 
+		return nombre; 
+		}
+	
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public void setTipoDeInstalacion(int tipo) { 
+    	this.tipoDeInstalacion = tipo; 
+    	}
 	
 }
